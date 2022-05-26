39f57436a5c3b72aeb883e1d78cc47a46c778f34
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2022-05-26 19:03:06 +0300

    javascript dom
    classwork with dom

---
+++ Strings/script.js
@@ -0,0 +1,8 @@
+const userInfoOutput = document.getElementById("main");
+let user = { firstName: "Rokas", lastName: "Misevicius" };
+userInfoOutput.innerHTML =
+  "<h2> User information </h2> <br/> <p> Vartotojo vardas : " +
+  user["firstName"] +
+  ", o pavarde " +
+  user["lastName"] +
+  "</p>";
