4a140786227efecfb020bb768182bedab865a304
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2022-05-26 19:32:37 +0300

    hot fix
    fix

--- Strings/script.js
+++ Strings/script.js
@@ -2,7 +2,7 @@ const userInfoOutput = document.getElementById("main");
 let user = { firstName: "Rokas", lastName: "Misevicius" };
 userInfoOutput.innerHTML =
   "<h2> User information </h2> <br/> <p> Vartotojo vardas : " +
-  user["firstName"] +
-  ", o pavarde " +
-  user["lastName"] +
-  "</p>";
+  ` ${user["firstName"]} 
+  , o pavarde 
+  ${user["lastName"]} 
+  </p>`;
