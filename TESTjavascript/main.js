f8b5ab08eab6e4f4140267996568e8d3bf5e4520
88ROKAS88 <102315648+88ROKAS88@users.noreply.github.com>
2022-06-30 22:07:50 +0300

    finished task
    fineshed work

--- TESTjavascript/main.js
+++ TESTjavascript/main.js
@@ -1,7 +1,14 @@
+// just some counters
+let firstCounter = 0;
+let secondCounter = 0;
+let thirdCounter = 0;
+let fourthCounter = 0;
+let fifthCounter = 0;
 // #### FIRST TASK ####
 const showAlertButton = document.getElementById("show-alert-button");
 showAlertButton.addEventListener("click", (e) => {
   alert("Hello World!");
+  firstCounter++;
 });
 // #### SECOND TASK ####
 const upperCaseInput = document.querySelector("#upper-case-input");
@@ -20,23 +27,27 @@ const firstToLowerCaseButton = document.getElementById(
 allToUpperCaseButton.addEventListener("click", function (e) {
   if (upperCaseInput.value) {
     upperCaseInput.value = upperCaseInput.value.toUpperCase();
+    secondCounter++;
   }
 });
 allToLowerCaseButton.addEventListener("click", function (e) {
   if (upperCaseInput.value) {
     upperCaseInput.value = upperCaseInput.value.toLowerCase();
+    secondCounter++;
   }
 });
 firstToUpperCaseButton.addEventListener("click", function (e) {
   if (upperCaseInput.value) {
     upperCaseInput.value =
       upperCaseInput.value[0].toUpperCase() + upperCaseInput.value.slice(1);
+    secondCounter++;
   }
 });
 firstToLowerCaseButton.addEventListener("click", function (e) {
   if (upperCaseInput.value) {
     upperCaseInput.value =
       upperCaseInput.value[0].toLowerCase() + upperCaseInput.value.slice(1);
+    secondCounter++;
   }
 });
 // #### THIRD TASK ####
@@ -48,16 +59,113 @@ const imputInvalid2 = document.querySelectorAll(".invalid-feedback")[1];
 const imputValid1 = document.querySelectorAll(".valid-feedback")[0];
 const imputValid2 = document.querySelectorAll(".valid-feedback")[1];
 
-function checkFormValue(imput, valid, invalid) {
+function showError(imput, valid, invalid) {
   if (imput.value) {
     valid.style.display = "block";
     invalid.style.display = "none";
+    imput.classList.remove("is-invalid");
   } else {
     invalid.style.display = "block";
     valid.style.display = "none";
+    imput.classList.add("is-invalid");
   }
 }
 sendForm.addEventListener("click", function () {
-  checkFormValue(emailImput, imputValid1, imputInvalid1);
-  checkFormValue(phoneNumber, imputValid2, imputInvalid2);
+  //email validation
+
+  if (emailImput.value.match(/\S+@\S+\.\S+/)) {
+    showError(emailImput, imputValid1, imputInvalid1);
+  } else {
+    emailImput.value = null;
+    showError(emailImput, imputValid1, imputInvalid1);
+  }
+
+  //phone number validation
+
+  if (phoneNumber.value > 860000000 && phoneNumber.value < 869999999) {
+    showError(phoneNumber, imputValid2, imputInvalid2);
+  } else {
+    phoneNumber.value = null;
+    showError(phoneNumber, imputValid2, imputInvalid2);
+  }
+
+  thirdCounter++;
+});
+
+// #### FOURTH TASK ####
+const blockButton = document.getElementById("block-button");
+const blockImputUnblock = document.getElementById("block-imput-unblock");
+const unblockButton = document.getElementById("unblock-button");
+
+blockButton.addEventListener("click", function () {
+  blockImputUnblock.disabled = true;
+  fourthCounter++;
+});
+unblockButton.addEventListener("click", function () {
+  blockImputUnblock.disabled = false;
+  fourthCounter++;
+});
+// #### FIFTH TASK is made with pure CSS ####
+
+// #### SIXTH TASK ####
+const experimentalText = document.querySelector("#experimental-text");
+const resetAllButton = document.querySelector("#reset-all-button");
+//https://stackoverflow.com/questions/7459704/in-javascript-what-is-the-best-way-to-convert-a-nodelist-to-an-array
+const redButtons = [...document.querySelectorAll(".red-button")];
+
+const propertyArray = [
+  "pointer",
+  "text",
+  "copy",
+  "foo",
+  "help",
+  "crosshair",
+  "red",
+  "green",
+  "blue",
+  "red",
+  "green",
+  "blue",
+];
+console.log(redButtons);
+for (let i in redButtons) {
+  if (i < 6) {
+    redButtons[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      document.body.style.cursor = propertyArray[i];
+      fifthCounter++;
+    });
+  } else if (i < 9) {
+    redButtons[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      experimentalText.style.color = propertyArray[i];
+      fifthCounter++;
+    });
+  } else if (i < 12) {
+    redButtons[i].addEventListener("click", function (e) {
+      e.preventDefault();
+      experimentalText.style.borderColor = propertyArray[i];
+      fifthCounter++;
+    });
+  }
+}
+resetAllButton.addEventListener("click", (e) => {
+  document.body.style.cursor = "auto";
+  experimentalText.style.color = "black";
+  experimentalText.style.borderColor = "#dee2e6";
+  fifthCounter++;
+});
+// #### SEVENTH TASK ####
+let statistics = `Number of times pressed on buttons: <br>
+1st task: ${firstCounter}
+2nd task: ${secondCounter}`;
+
+const statisticsElement = document.getElementById("statistics");
+document.getElementById("modal").addEventListener("click", () => {
+  statisticsElement.innerHTML = `Number of times pressed on buttons: <br/>
+  1st task: ${firstCounter} <br/>
+  2nd task: ${secondCounter} <br/>
+  3rd task: ${thirdCounter} <br/>
+  4th task: ${fourthCounter} <br/>
+  6th task: ${fifthCounter}`;
 });
